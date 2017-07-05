016c85a24c8ea0e0f23c9aa1429211c1c798065c
Jordan Gregory <jordangregory@gmail.com>
2017-07-05 16:06:31 -0500

    added signup info to database

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const authRouter = express.Router();
+const models = require("../models");
+
 // render login page
 authRouter.get("/login", function(req, res) {
   res.render("login");
 });
 // log in the user
 authRouter.post("/login", function(req, res) {
-  res.render("login");
-});
-
-// render signup page
-authRouter.get("/signup", function(req, res) {
-  res.render("signup");
+  return res.redirect("/login");
 });
 
 // create a new user
 authRouter.post("/signup", function(req, res) {
+  var newUser = models.user
+    .build({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      bio: req.body.bio
+    })
+    .save()
+    .then(function(savedUser) {})
+    .catch(function(err) {
+      console.warn("ERROR ", err);
+    });
+  return res.redirect("/auth/login");
+});
+
+// render signup page
+authRouter.get("/signup", function(req, res) {
   res.render("signup");
 });
 
