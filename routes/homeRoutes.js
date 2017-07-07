a6e41b2680aa3b70ad5463669875bfff29e133a4
Jordan Gregory <jordangregory@gmail.com>
2017-07-06 19:16:19 -0500

    rendered gabs on homepage

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const homeRouter = express.Router();
+const models = require("../models");
 
 homeRouter.get("/", function(req, res) {
-  // query posts for most recent posts
+  models.post.findAll().then(function(foundPosts) {
+    console.log("foundPosts: ", foundPosts);
+    res.render("homepage", { gabs: foundPosts });
+  }); // query posts for most recent posts
   // join related posts
   // render homepage with posts and likes
-  //const queryEndResult
   console.log("got home route");
-  res.render(
-    "homepage",
-    {
-      // gabs: queryEndResult
-    }
-  );
   // res.send("got it");
 });
 
